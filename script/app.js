375858b1ce310c5c4df6f8fdf08f74a6d225c558
Skyggfisk <mahasi93@hotmail.com>
2018-11-14 18:41:16 +0100

    Doing this

--- script/app.js
+++ script/app.js
@@ -2,16 +2,17 @@
 const wavesurfer = WaveSurfer.create({
   container: "#waveform",
   waveColor: "violet",
-  progressColor: "purple"
+  progressColor: "purple",
+  responsive: true
 });
 
-// autoload test file
+// autoload test file and set volume to 50%
 wavesurfer.load("audio/Vivaldi-The_Four_Seasons/01_-_Spring_Mvt_1_Allegro.mp3");
+wavesurfer.setVolume(0.5);
 
 // upload track to play
 document.querySelector("#upload-track").addEventListener("change", function() {
   let trackUrl = URL.createObjectURL(this.files[0]);
-  console.log(trackUrl);
   wavesurfer.load(trackUrl);
 });
 
@@ -43,6 +44,7 @@ document
     wavesurfer.toggleMute();
   });
 
+// stop-button event handler
 document.querySelector(".stop-button").addEventListener("click", function() {
   wavesurfer.stop();
   if (wavesurfer.isPlaying()) {
@@ -50,4 +52,11 @@ document.querySelector(".stop-button").addEventListener("click", function() {
     pp.classList.remove("fa-pause");
     pp.classList.add("fa-play");
   }
+});
+
+// volume-slider event handler
+document.querySelector(".volume-slider").addEventListener("change", function() {
+  wavesurfer.setVolume(this.value / 100);
+  let voltext = document.querySelector(".volume-text");
+  voltext.innerHTML = `${this.value}%`;
 });
