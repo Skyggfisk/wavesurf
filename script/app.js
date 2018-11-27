4fde43d6094f50290b5ead6f4d53381ac8a4c40d
Skyggfisk <mahasi93@hotmail.com>
2018-11-27 18:09:52 +0100

    Add to playlist eventhandler now gets song name and song duration.

--- script/app.js
+++ script/app.js
@@ -6,41 +6,55 @@ const wavesurfer = WaveSurfer.create({
   responsive: true
 });
 
+// global used to pass song name from upload handler to hidden audio
+// I did this because I have no idea how to get the file name from a blob / HTMLAudioElement's src
+var songName = "";
+
 // autoload test file and set volume to 50%
 wavesurfer.load("audio/Vivaldi-The_Four_Seasons/01_-_Spring_Mvt_1_Allegro.mp3");
 wavesurfer.setVolume(0.5);
 
-// upload track to play
+// upload track to play and add to playlist
 document.querySelector("#upload-track").addEventListener("change", function() {
   let trackUrl = URL.createObjectURL(this.files[0]);
-  console.log(trackUrl);
   wavesurfer.load(trackUrl);
 
-  // create and add new song element to the playlist
-  // let song = document.createElement("div");
-  // song.classList.add("song");
-  // let songname = document.createElement("p");
-  // songname.classList.add("song-name");
-  // let songlength = document.createElement("p");
-  // songlength.classList.add("song-length");
+  // get file duration, refer to event handler for more details
+  let audio = document.getElementById("hidden-audio");
+  audio.src = trackUrl;
+  audio.load();
 
-  // songname.innerHTML("en sang");
-  // songlength.innerHTML("0:00");
-
-  let songhtml = "";
-  songhtml +=
-    "<div class='song'><p class='song-name>en sang</p><p class='song-length'>0:00</p></div>";
-
-  let playlist = document.getElementsByClassName("playlist");
-
-  // document.querySelector(".playlist").insertAdjacentHTML(
-  //   beforeend,
-  //   `<div class="song">
-  //       <p class="song-name">${trackUrl}</p>
-  //       <p class="song-length">0:00</p>
-  //     </div>`
-  // );
+  // Avert your eyes!
+  songName = this.files[0].name;
 });
+
+// hidden audio event-handler, used to get song duration
+// modified version of https://jsfiddle.net/derickbailey/s4P2v/
+document
+  .getElementById("hidden-audio")
+  .addEventListener("canplaythrough", function() {
+    let seconds = this.duration;
+    let duration = moment.duration(seconds, "seconds");
+
+    var time = "";
+    let hours = duration.hours();
+    if (hours > 0) {
+      time = hours + ":";
+    }
+
+    time = time + duration.minutes() + ":" + duration.seconds();
+
+    console.log("LOADED FILE: " + this.src + "\n DURATION: " + time);
+
+    // insert new track in the playlist
+    document.getElementById("playlist").insertAdjacentHTML(
+      "beforeend",
+      `<div class="song">
+        <p class="song-name">${songName}</p>
+        <p class="song-length">${time}</p>
+      </div>`
+    );
+  });
 
 // play-pause event handler
 document
