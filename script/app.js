741408b28d02a6f5a6e0eefd89c0fbb77a10d383
Skyggfisk <mahasi93@hotmail.com>
2018-11-28 18:25:41 +0100

    Move eventhandlers to own functions. Can add files to idb.

--- script/app.js
+++ script/app.js
@@ -1,18 +1,18 @@
-// global used to pass song name from upload handler to hidden audio
+// global song obj used to pass info from upload handler to hidden audio
 // I did this because I have no idea how to get the file name from a blob / HTMLAudioElement's src
-let songName;
+let song = { title: "", data: "", duration: "" };
 let db;
 
-window.onload = function() {
-  // setup wavesurfer
-  const wavesurfer = WaveSurfer.create({
-    container: "#waveform",
-    waveColor: "darkorchid",
-    progressColor: "purple",
-    responsive: true
-  });
+// setup wavesurfer
+const wavesurfer = WaveSurfer.create({
+  container: "#waveform",
+  waveColor: "darkorchid",
+  progressColor: "purple",
+  responsive: true
+});
 
-  // IDB functionality
+window.onload = function() {
+  // idb functionality
   // https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Client-side_web_APIs/Client-side_storage
   // request to open db called playlist v1.0
   let req = window.indexedDB.open("playlist", 1);
@@ -32,125 +32,150 @@ window.onload = function() {
   req.onupgradeneeded = function(e) {
     let db = e.target.result;
 
-    db.createObjectStore("songs", {
+    let objectStore = db.createObjectStore("songs", {
       keyPath: "id",
       autoIncrement: true
     });
 
-    // Define what data items the objectStore will contain
-    // objectStore.createIndex("title", "title", { unique: false });
-    // objectStore.createIndex("duration", "duration", { unique: false });
-    // objectStore.createIndex("data");
+    // create indexes defining the 'schema'
+    objectStore.createIndex("title", "title", { unique: false });
+    objectStore.createIndex("duration", "duration", { unique: false });
+    objectStore.createIndex("data", "data", { unique: false });
 
     console.log("Database setup complete");
   };
 
-  // upload track to play and add to playlist
+  // set event handlers
+  uploadTrackEventHandler();
+  hiddenAudioEventHandler();
+  playPauseEventHandler();
+  muteEventHandler();
+  stopButtonEventHandler();
+  volumeEventHandler();
+};
+
+// upload track event handler
+function uploadTrackEventHandler() {
   document
     .querySelector("#upload-track")
     .addEventListener("change", function() {
-      let song = URL.createObjectURL(this.files[0]);
-      wavesurfer.load(song);
+      let songObj = URL.createObjectURL(this.files[0]);
+      wavesurfer.load(songObj);
+      console.log(this.files[0]);
 
-      // let testObj = { title: "hello", body: "this is a body" };
-      console.log(song);
-      let trx = db.transaction(["songs"], "readwrite");
-      let objStore = trx.objectStore("songs");
-      var request = objStore.add(this.files[0]);
-      request.onsuccess = function() {
-        console.log("something happened");
-      };
+      song.data = this.files[0];
+      song.title = this.files[0].name;
 
-      trx.oncomplete = function() {
-        console.log("Transaction completed. Added something.");
-      };
-
-      trx.onerror = function() {
-        console.log("Transaction error.");
-      };
-
-      // add file to playlist, refer to event handler for more details
+      // add file to playlist, refer to hidden audio event handler for more details
       let audio = document.getElementById("hidden-audio");
-      audio.src = trackUrl;
+      audio.src = songObj;
       audio.load();
-
-      // Avert your eyes!
-      songName = this.files[0].name;
     });
-};
+}
+
+// opens a transaction and adds the given track to the "songs" store
+function addToIDB(track) {
+  let trx = db.transaction(["songs"], "readwrite");
+  let objStore = trx.objectStore("songs");
+  objStore.add(track);
+
+  // log transaction completion
+  trx.oncomplete = () => {
+    console.log(`Transaction completed. Added ${track.title}.`);
+  };
+
+  // log transaction error
+  trx.onerror = () => {
+    console.log("Transaction error.");
+  };
+}
 
 // hidden audio event-handler, used to get song duration
 // modified version of https://jsfiddle.net/derickbailey/s4P2v/
-document
-  .getElementById("hidden-audio")
-  .addEventListener("canplaythrough", function() {
-    let seconds = this.duration;
-    let duration = moment.duration(seconds, "seconds");
+function hiddenAudioEventHandler() {
+  document
+    .getElementById("hidden-audio")
+    .addEventListener("canplaythrough", function() {
+      let seconds = this.duration;
+      let duration = moment.duration(seconds, "seconds");
 
-    var time = "";
-    let hours = duration.hours();
-    if (hours > 0) {
-      time = hours + ":";
-    }
+      let time = "";
+      let hours = duration.hours();
+      if (hours > 0) {
+        time = hours + ":";
+      }
 
-    time = time + duration.minutes() + ":" + duration.seconds();
+      time = time + duration.minutes() + ":" + duration.seconds();
+      song.duration = time;
 
-    console.log("LOADED FILE: " + this.src + "\n DURATION: " + time);
+      // add to song store, now that we have all the data
+      addToIDB(song);
 
-    // insert new track in the playlist
-    document.getElementById("playlist").insertAdjacentHTML(
-      "beforeend",
-      `<div class="song">
-        <p class="song-name">${songName}</p>
-        <p class="song-length">${time}</p>
-      </div>`
-    );
-  });
+      console.log("LOADED FILE: " + this.src + "\n DURATION: " + time);
 
-// document.querySelector(".song-name");
+      // insert new track in the playlist
+      document.getElementById("playlist").insertAdjacentHTML(
+        "beforeend",
+        `<div class="song" id="${1}">
+          <p class="song-name">${song.title}</p>
+          <p class="song-length">${time}</p>
+        </div>`
+      );
+    });
+}
 
 // play-pause event handler
-document
-  .querySelector(".play-pause-button")
-  .addEventListener("click", function() {
-    if (!wavesurfer.isPlaying()) {
-      this.classList.remove("fa-play");
-      this.classList.add("fa-pause");
-    } else {
-      this.classList.remove("fa-pause");
-      this.classList.add("fa-play");
-    }
-    wavesurfer.playPause();
-  });
+function playPauseEventHandler() {
+  document
+    .querySelector(".play-pause-button")
+    .addEventListener("click", function() {
+      if (!wavesurfer.isPlaying()) {
+        this.classList.remove("fa-play");
+        this.classList.add("fa-pause");
+      } else {
+        this.classList.remove("fa-pause");
+        this.classList.add("fa-play");
+      }
+      wavesurfer.playPause();
+    });
+}
 
 // mute-unmute event handler
-document
-  .querySelector(".mute-unmute-button")
-  .addEventListener("click", function() {
-    if (!wavesurfer.getMute()) {
-      this.classList.remove("fa-volume-down");
-      this.classList.add("fa-volume-mute");
-    } else {
-      this.classList.remove("fa-volume-mute");
-      this.classList.add("fa-volume-down");
-    }
-    wavesurfer.toggleMute();
-  });
+function muteEventHandler() {
+  document
+    .querySelector(".mute-unmute-button")
+    .addEventListener("click", function() {
+      if (!wavesurfer.getMute()) {
+        this.classList.remove("fa-volume-down");
+        this.classList.add("fa-volume-mute");
+      } else {
+        this.classList.remove("fa-volume-mute");
+        this.classList.add("fa-volume-down");
+      }
+      wavesurfer.toggleMute();
+    });
+}
 
 // stop-button event handler
-document.querySelector(".stop-button").addEventListener("click", function() {
-  wavesurfer.stop();
-  if (wavesurfer.isPlaying()) {
-    // TODO this doesnt change pause to play when stop button is clicked
-    let pp = document.querySelector(".play-pause-button");
-    pp.classList.remove("fa-pause");
-    pp.classList.add("fa-play");
-  }
-});
+function stopButtonEventHandler() {
+  document.querySelector(".stop-button").addEventListener("click", function() {
+    wavesurfer.stop();
+    if (wavesurfer.isPlaying()) {
+      // TODO this doesnt change pause to play when stop button is clicked
+      let pp = document.querySelector(".play-pause-button");
+      pp.classList.remove("fa-pause");
+      pp.classList.add("fa-play");
+    }
+  });
+}
 
 // volume-slider event handler
-document.querySelector(".volume-slider").addEventListener("change", function() {
-  wavesurfer.setVolume(this.value / 100);
-  let voltext = document.querySelector(".volume-text");
-  voltext.innerHTML = `${this.value}%`;
-});
+function volumeEventHandler() {
+  document
+    .querySelector(".volume-slider")
+    .addEventListener("change", function() {
+      wavesurfer.setVolume(this.value / 100);
+      let voltext = document.querySelector(".volume-text");
+      voltext.innerHTML = `${this.value}%`;
+    });
+}
